559914fade789e6e9a5e9ded33c8f59b87d9d370
caleb <CMaclean98@gmail.com>
2024-07-15 21:48:42 -0700

    Updated sql.js, created function for insertion queries that don't require all fields to have values.

--- server/helpers/sql.js
+++ server/helpers/sql.js
@@ -32,4 +32,46 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   };
 }
 
-module.exports = { sqlForPartialUpdate };
+/** sqlForPartialInsert helper function
+ * Helper for making selective insert statements.
+ * The calling function can use it to make the INSERT and VALUES clause.
+ * 
+ * Parameters:
+ * dataToInsert: obj - {field: val, field2: val, ...}
+ * jsToSql: obj - maps js-style data fields to database column names {camelCase: col_name, ...}
+ * 
+ * Returns: obj - { insertColumns, valuesIndecies }
+ */
+function sqlForPartialInsert(dataToInsert, jsToSql) {
+  // create keys array from dataToInsert
+  const keys = Object.keys(dataToInsert);
+
+  // if no data throw BadRequestError
+  // if( keys.length === 0 ) throw new BadRequestError("No data");
+
+  // create arrays to store the converted column names and there variable indexes;
+  const cols = [];
+  const valIdxArr = [];
+
+  // iterate through keys and push values to respective arrays
+  keys.forEach((colName, idx) => {
+      // convert if necessary
+      let properCol = jsToSql[colName] ? jsToSql[colName] : colName;
+      let valIdx = "$" + (idx + 1);
+      // push values to arrays
+      cols.push(properCol);
+      valIdxArr.push(valIdx);
+  });
+
+  // console.log("columns: ", cols.join(', '));
+  // console.log("indecies: ", valIdxArr.join(', '));
+  // console.log("values: ", Object.values(dataToInsert));
+
+  return {
+      insertColumns: cols.join(', '),
+      valuesIndecies: valIdxArr.join(', '),
+      values: Object.values(dataToInsert)
+  }
+}
+
+module.exports = { sqlForPartialUpdate, sqlForPartialInsert };
