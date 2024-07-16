cb9e5cc29d95e8bbda5cdb962c1183edbfb98b16
CalebMaclean <CMaclean98@gmail.com>
2024-07-16 02:37:43 -0700

    Created models/group.js, created Group query class that has crud methods for groups table; Created models/group.test.js, created testing for all methods found in Groups.

--- server/airdnd-schema.sql
+++ server/airdnd-schema.sql
@@ -24,8 +24,8 @@ CREATE TABLE groups (
 );
 
 CREATE TABLE members (
-    user_id VARCHAR(25) REFERENCES users(username),
-    group_id INT REFERENCES groups(id),
+    user_id VARCHAR(25) REFERENCES users(username) ON DELETE CASCADE,
+    group_id INT REFERENCES groups(id) ON DELETE CASCADE,
     PRIMARY KEY (user_id, group_id),
     is_accepted BOOLEAN DEFAULT FALSE,
     is_dm BOOLEAN DEFAULT FALSE
