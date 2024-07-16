ea42754c3d44cec07a668348b4903fe7755d2197
The-Malcmann <malcolmateel2@gmail.com>
2024-07-15 21:06:26 -0700

    user model

---
+++ server/models/user.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { sqlForPartialUpdate } = require("../helpers/sql");
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../expressError");
+
+const { BCRYPT_WORK_FACTOR } = require("../config.js");
+
+/** Related functions for users. */
+
+class User {
+    /** Register user with data.
+   *
+   * Returns { username, firstName, lastName, email, isAdmin }
+   *
+   * Throws BadRequestError on duplicates.
+   **/
+
+  static async register(
+    { username, password, email, isAdmin }) {
+  const duplicateCheck = await db.query(
+        `SELECT username
+         FROM users
+         WHERE username = $1`,
+      [username],
+  );
+
+  if (duplicateCheck.rows[0]) {
+    throw new BadRequestError(`Duplicate username: ${username}`);
+  }
+
+  const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+  const result = await db.query(
+        `INSERT INTO users
+         (username,
+          password,
+          email,
+          is_admin)
+         VALUES ($1, $2, $3, $4, $5, $6)
+         RETURNING username, email, is_admin AS "isAdmin"`,
+      [
+        username,
+        hashedPassword,
+        email,
+        isAdmin,
+      ],
+  );
+
+  const user = result.rows[0];
+
+  return user;
+}
+
+}
