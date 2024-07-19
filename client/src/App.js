af10023b45e4a620b9d3da26e153df5fc9ed73f2
The-Malcmann <malcolmateel2@gmail.com>
2024-07-19 17:53:56 -0600

    login works

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <nav style={{ margin: 10 }}>
-        <Link to="/groups" style={{ padding: 5 }}>
+        <Link to="/" style={{ padding: 5 }}>
           Groups
         </Link>
         <Link to="/profile" style={{ padding: 5 }}>
@@ -23,11 +23,15 @@ function App() {
         <Link to="/register" style={{ padding: 5 }}>
           Register
         </Link>
+        <Link to="/login" style={{ padding: 5 }}>
+          Login
+        </Link>
       </nav>
       <Routes>
-        <Route path="/groups" element={<Groups />} />
+        <Route path="/" element={<Groups />} />
         <Route path="/profile" element={<Profile username={username} />} />
         <Route path="/register" element={<Register login={handleLogin} />} />
+        <Route path="/login" element={<Login login={handleLogin} />} />
       </Routes>
     </Router>
   );
@@ -68,14 +72,27 @@ const Profile = ({ username }) => {
     async function getUser(username) {
       console.log(token)
       console.log(username)
-      const res = await axios.get(`/users/${username}`, { headers: { Authorization: `Bearer ${token}`}});
+      const res = await axios.get(`/users/${username}`, { headers: { Authorization: `Bearer ${token}` } });
       setData(res.data)
+      console.log(res.data)
     }
     getUser(username)
   }, []);
   return (
     <section>
       <div>Profile</div>
+      <div>
+        {data ? 
+        <div>
+          <h1>Username: {data.user.username}</h1> 
+          <h2>email: {data.user.email}</h2> 
+          <h2>prefRemote: {data.user.prefRemote? "true" : "false"}</h2>
+          <h2>prefInPerson: {data.user.prefInPerson? "true" : "false"}</h2>
+          <h2>Can DM? {data.user.canDm? "true" : "false"}</h2>
+        </div> : 
+        <div></div>}
+        
+      </div>
     </section>
   )
 }
@@ -101,7 +118,7 @@ const Register = ({ login }) => {
       const token = res.data.token
       login(user, token);
       setFormData(INITIAL_STATE);
-      // navigate("/groups");
+      navigate("/");
     } catch (err) {
       console.log(err)
     }
@@ -112,6 +129,44 @@ const Register = ({ login }) => {
         <input type="text" placeholder="username" name="username" onChange={handleChange} />
         <input type="text" placeholder="password" name="password" onChange={handleChange} />
         <input type="email" placeholder="email" name="email" onChange={handleChange} />
+        <button type="submit" />
+      </form>
+    </section>
+  )
+}
+const Login = ({ login }) => {
+  const INITIAL_STATE = { username: "", password: "" }
+  const [formData, setFormData] = useState(INITIAL_STATE)
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(data => ({
+      ...data,
+      [name]: value
+    }));
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { username, password } = formData;
+      const user = { username: formData.username };
+      const res = await axios.post('/auth/token', formData);
+      const token = res.data.token
+      login(user, token);
+      setFormData(INITIAL_STATE);
+      navigate("/");
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  return (
+    <section>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="username" name="username" onChange={handleChange} />
+        <input type="text" placeholder="password" name="password" onChange={handleChange} />
+        {/* <input type="email" placeholder="email" name="email" onChange={handleChange} /> */}
         <button type="submit" />
       </form>
     </section>
