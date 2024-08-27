cf1252c753656985ab386f69c30489209e6ae3c2
caleb <CMaclean98@gmail.com>
2024-08-26 18:02:29 -0700

    added notes to help understand what is happening in Messages.js

--- client/src/messages/Messages.js
+++ client/src/messages/Messages.js
@@ -4,32 +4,40 @@ import { useCreateChatClient, Chat, Channel, ChannelList, ChannelHeader, Message
 import { AuthContext } from '../auth';
 import './Messages.css'
 
+/** */
 const Messages = () => {
-
+    // State
     const [channel, setChannel] = useState()
     const [filters, setFilters] = useState()
     const [users, setUsers] = useState()
     const { client, username, token } = useContext(AuthContext);
-    
+    // sorting criteria for channels
     const sort = { last_message_at: -1 };
-
+    // options for channel query, limit to 10 channels
     const options = {
         limit: 10,
     };
-
+    // Effect: fetch users and set up chat channel
     useEffect(() => {
+        // catch cases where client, username or token are not set
         if (!client || !username || !token) return
+
+        // fetch users and store in state
         const getUsers = async() => {
             const res = await axios.get(`/users/`, { headers: { Authorization: `Bearer ${token}` }} )
             setUsers(res.data.users)
         }
         getUsers()
+        // catch cases where users are not set
         if(!users) return
+
+        // create a channel for messaging, hardcoding the first two users
         const channel = client.channel('messaging', {
             image: 'https://getstream.io/random_png/?name=react',
             name: 'Talk about React',
             members: [users[0].username, users[1].username],
         });
+        // set filters for channel list
         const filters = {
             type: 'messaging',
             members: { $in: [username] },
@@ -39,6 +47,7 @@ const Messages = () => {
         setFilters(filters)
     }, [users])
 
+    // if client is not set, return a loading message
     if (!client) return <div>Setting up client & connection...</div>;
 
     return (
